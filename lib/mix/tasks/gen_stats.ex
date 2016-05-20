a828e04e07578d3d6c00d28dee7fb8278a8558fa
Bryan Weber <bryan@cobenian.com>
2016-05-20 14:48:10 -0400

    saving code, need to get data and template next

--- lib/mix/tasks/gen_stats.ex
+++ lib/mix/tasks/gen_stats.ex
@@ -13,7 +13,26 @@ defmodule Mix.Tasks.Gen.Stats do
 
   def gen_service({service}), do: gen_service({service,true})
   def gen_service({service,name_first}) do
+    gen_stats(service,name_first)
+  end
 
+  # same code from template example...
+  def gen_stats(service,name_first,module_dir \\ "lib/generated") do
+    :ok = File.mkdir_p!(module_dir)
+    code = EEx.eval_file("lib/templates/stats_template.eex", [assigns: [service_name: service, name_first: name_first]])
+    filename = Path.join([module_dir, String.downcase(service) <> ".ex"])
+    :ok = File.write!(filename, code)
+    IO.puts "Generated: #{filename}"
+  end
+
+  def flag(str) do
+    case String.downcase(str) do
+      "true" -> true
+      "t" -> true
+      "yes" -> true
+      "y" -> true
+      _ -> false
+    end
   end
 
 end
