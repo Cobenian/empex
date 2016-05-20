969961914d6e71dc09533a5aefd34c1954c096d6
Bryan Weber <bryan@cobenian.com>
2016-05-20 14:51:13 -0400

    data model example

--- lib/mix/tasks/gen_stats.ex
+++ lib/mix/tasks/gen_stats.ex
@@ -8,12 +8,12 @@ defmodule Mix.Tasks.Gen.Stats do
 
   def run(args) do
     Application.get_env(:code_gen, :data)[:services]
-    Enum.map(&gen_service/1)
+    |> Enum.map(&gen_service/1)
   end
 
   def gen_service({service}), do: gen_service({service,true})
   def gen_service({service,name_first}) do
-    gen_stats(service,name_first)
+    gen_stats(Atom.to_string(service),name_first)
   end
 
   # same code from template example...
